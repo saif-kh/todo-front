643a808c1bd1b627dd1fe1b4ae9e59ecdb400153
s.elkharraz <saif.elkharraz@seven-app.net>
2026-03-16 16:34:11 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+function App() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    async function getTodos() {
+      const res = await fetch("http://localhost:8080/api/todos/all");
+      const data = await res.json();
+      setData(data);
+    }
+    getTodos();
+  }, []);
+
+  return <div>hi {data}</div>;
+}
+
+export default App;
