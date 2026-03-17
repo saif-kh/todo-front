d11d4ba824a440a5e75ebe1166b78fa4973b4f0a
saif-kh <fiaskharraz@gmail.com>
2026-03-17 23:15:58 +0000

    feat:add delete and update apis

--- src/constants/ApiUrls.ts
+++ src/constants/ApiUrls.ts
@@ -1,3 +1,5 @@
 const BASE_URL = "http://localhost:8080/api/todos";
 export const GET_TODOS = BASE_URL + "/all";
 export const ADD_TODO = BASE_URL + "/add";
+export const CHANGE_TODO = BASE_URL + "/";
+export const DELETE_TODO = BASE_URL + "/";
